0e6ea0ad79c4df133bb739f8ab876353c064c099
shaxzod0022 <ilgeldiyevshaxzod@gmail.com>
2025-08-06 17:17:32 +0000

    layout2

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -21,7 +21,7 @@ export async function generateMetadata({
   const descriptions: Record<string, string> = {
     en: "Explore the world with Centralia Travel Agency.",
     es: "Descubre viajes inolvidables con Centralia Travel Agency. Explora tours personalizados, experiencias culturales y viajes sin complicaciones a los mejores destinos del mundo. ¡Reserva tu próxima aventura hoy mismo!",
-    fr: "Découvrez des voyages inoubliables avec Centralia Travel Agency. Profitez de circuits personnalisés, d’expériences culturelles et d’un voyage sans stress vers les meilleures destinations du monde. Réservez votre prochaine aventure dès aujourd'hui !",
+    fr: "Découvrez des voyages inoubliables avec Centralia Travel Agency. Profitez de circuits personnalisés, d'expériences culturelles et d'un voyage sans stress vers les meilleures destinations du monde. Réservez votre prochaine aventure dès aujourd'hui !",
     zh: "与 Centralia 旅行社一起开启难忘的旅程。探索个性化旅行、文化体验以及前往全球热门目的地的无忧旅行。立即预订您的下一次冒险=！",
   };
 
@@ -36,14 +36,18 @@ type LocaleLayoutProps = {
   params: { locale: string };
 };
 
-export default async function LocaleLayout({ children, params }: LocaleLayoutProps) {
-  const { locale } = params;
+export default async function LocaleLayout({
+  children,
+  params: { locale },
+}: {
+  children: React.ReactNode;
+  params: { locale: string };
+}) {
   const messages = await getMessages({ locale });
 
   return (
     <html lang={locale} suppressHydrationWarning className="hydrate">
       <head>
-        {/* Google Fontsni shu yerda link qilib qo'yamiz */}
         <link
           href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
           rel="stylesheet"
